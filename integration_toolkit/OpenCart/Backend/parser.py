3b3602b5b56438b5bdccf2443db225942efd223f
Афанасьев Иван <afanasieffivan@gmail.com>
2026-02-22 05:07:48 +0300

    chore: clean up root directory
    - Remove all moved files from root directory
    - Clean repository structure with only essential files remaining
    - All source code now properly organized in src/, scripts/, and test/

--- integration_toolkit/OpenCart/Backend/parser.py
+++
@@ -1,9 +0,0 @@
-# JSON data parser 
-import json
-
-try:
-    with open()
-
-print (type(str_json))
-data = json.loads(str_json)
-print(data)
--- integration_toolkit/OpenCart/ProductExport/upload/catalog/language/en-gb/extension/module/product_data_export.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading
-$_['heading_title']         = 'Product Data Export';
-
-// Text
-$_['text_export']           = 'Export Products';
